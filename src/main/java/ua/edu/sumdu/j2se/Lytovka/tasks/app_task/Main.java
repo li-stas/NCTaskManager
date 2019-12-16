a3d8bbce97e7a2ca67a76aa4feb5c842d6212ca9
lista <lista@bk.ru>
2019-12-16 20:34:48 +0200

    Добавление заданий

--- src/main/java/ua/edu/sumdu/j2se/Lytovka/tasks/app_task/Main.java
+++ src/main/java/ua/edu/sumdu/j2se/Lytovka/tasks/app_task/Main.java
@@ -7,31 +7,32 @@ import ua.edu.sumdu.j2se.lytovka.tasks.view.TasksView;
 
 
 public class Main {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         /*Scanner scan = new Scanner(System.in);
         MenuMain.processChoice();
         MenuMain.nChoice(scan);*/
-       doMngTask();
+        doMngTask();
 
     }
-    private static void doMngTask()  {
+
+    private static void doMngTask() {
         int choice;
-       ArrayTaskList model = new ArrayTaskList();
+        ArrayTaskList model = new ArrayTaskList();
         TasksView view = new TasksView();
-        TasksCtrl ctrl = new TasksCtrl( model, view );
+        TasksCtrl ctrl = new TasksCtrl(model, view);
 
         while (true) {
 
-         ctrl.ShowTasks(); // вывести список задач
+            ctrl.ShowTasks(); // вывести список задач
 
             choice = view.menu00();
             //System.out.printf("choice=%d\n",choice);
-            if (choice == 0){
+            if (choice == 0) {
                 break;
             }
-            //RunEntry entry = (RunEntry) ctrl.CtrlMenu00().getEntries().get(choice - 1);
-            //entry.run();
-            ((RunEntry) ctrl.CtrlMenu00().getEntries().get(choice - 1)).run();
+            RunEntry entry = (RunEntry) ctrl.CtrlMenu00().getEntries().get(choice - 1);
+            entry.run();
+            //((RunEntry) ctrl.CtrlMenu00().getEntries().get(choice - 1)).run();
         }
-     }
+    }
 }
--- src/main/java/ua/edu/sumdu/j2se/Lytovka/tasks/controller/RunEntry.java
+++ src/main/java/ua/edu/sumdu/j2se/Lytovka/tasks/controller/RunEntry.java
@@ -1,13 +1,11 @@
 package ua.edu.sumdu.j2se.lytovka.tasks.controller;
 
 public abstract class RunEntry {
-    private int curNumEntry;
-
+    private int  curNumEntry;
 
     public RunEntry(int i) {
-        this.curNumEntry = i;
+         this.curNumEntry = i;
     }
-
 
     public int getCurNumEntry() {
         return curNumEntry;
