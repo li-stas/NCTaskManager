ea70fc6485cd3d9e338c7281044713e03a513295
lista <lista@bk.ru>
2019-12-17 09:12:47 +0200

    Обновлены директории

--- src/main/java/ua/edu/sumdu/j2se/lytovka/tasks/app_task/Main.java
+++ src/main/java/ua/edu/sumdu/j2se/lytovka/tasks/app_task/Main.java
@@ -17,13 +17,14 @@ public class Main {
         ArrayTaskList model = new ArrayTaskList();
         TasksView view = new TasksView();
         TasksCtrl ctrl = new TasksCtrl(model, view);
-
         while (true) {
+
             ctrl.ShowTasks();  // вывести список задач
             choice = view.menu00();
             if (choice == 0) break;
             RunEntry entry = ctrl.Menu00(choice); // выбор методов
             entry.run();
+
         }
     }
 }
