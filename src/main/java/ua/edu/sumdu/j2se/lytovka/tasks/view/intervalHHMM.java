5e9e6ab43af9d8d6d7498db19e612404729bd7e0
lista <lista@bk.ru>
2019-12-30 11:48:18 +0200

    обновлена д-ция, контроллер разнесен по классам

---
+++ src/main/java/ua/edu/sumdu/j2se/lytovka/tasks/view/intervalHHMM.java
@@ -0,0 +1,7 @@
+package ua.edu.sumdu.j2se.lytovka.tasks.view;
+
+public class intervalHHMM {
+    public String get_intervalHHMM(int time) {
+        return String.format("%02d:%02d:%02d", time / 3600, time / 60 % 60, time % 60);
+    }
+}
